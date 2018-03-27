34353a9e9f3400df1ccf698d877d5d18d912355a
Eduardo Galego <egalego@ext.leroymerlin.com.br>
2018-03-27 14:56:31 -0300

    add new features

---
+++ src/main/java/fiap/scj/restful/ws/resources/bean/Message.java
@@ -0,0 +1,30 @@
+package fiap.scj.restful.ws.resources.bean;
+
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Message {
+
+	private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	private Date dataCreation;
+
+	public Date getDataCreation() {
+		return dataCreation;
+	}
+
+	public void setDataCreation(Date dataCreation) {
+		this.dataCreation = dataCreation;
+	}
+
+}
