3e62fbb7871f39d1d13309b326e618d4e90d6c5e
Eduardo Galego <efgalego@ime.usp.br>
2018-04-19 14:59:36 -0300

    Add basic authentication

---
+++ src/main/java/fiap/scj/restful/ws/security/RestAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package fiap.scj.restful.ws.security;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RestAuthenticationFilter implements Filter {
+
+	private static final String AUTHENTICATION_HEADER = "Authorization";
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filter)
+			throws IOException, ServletException {
+
+		if (request instanceof HttpServletRequest) {
+			HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+			String authCredentials = httpServletRequest.getHeader(AUTHENTICATION_HEADER);
+
+			AuthenticationService service = new AuthenticationService();
+			boolean authenticationStatus = service.authenticate(authCredentials);
+
+			if (authenticationStatus) {
+				filter.doFilter(request, response);
+			} else {
+				if (response instanceof HttpServletResponse) {
+					HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+					httpServletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					httpServletResponse.getWriter().write("Acesso não permitido");
+					httpServletResponse.getWriter().flush();
+					httpServletResponse.getWriter().close();
+				}
+			}
+		}
+	}
+
+	public void destroy() {
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
